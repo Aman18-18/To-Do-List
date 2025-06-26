3fc5e50c3c5d26181d9a4cf77bfcbe7c0666e8e1
Aman18-18 <amanssingh1827@gmail.com>
2025-06-26 10:17:29 +0530

    make the delete link interactive

--- HTML-CSS-JS/AMAZON-CLONE/data/cart.js
+++ HTML-CSS-JS/AMAZON-CLONE/data/cart.js
@@ -1,4 +1,8 @@
- export const cart =[{
+ export let cart = JSON.parse(localStorage.getItem('cart'));
+ 
+ 
+ if(!cart){
+    cart =  [{
     productId : 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
     quantity : 2,
  },
@@ -7,6 +11,12 @@
     quantity: 1,
 }
 ];
+  }
+
+
+function saveToStorage(){
+    localStorage.setItem('cart',JSON.stringify(cart));
+}
 
 export function productQuantity(productId){
      let matchingItem;
@@ -25,4 +35,18 @@ export function productQuantity(productId){
                      quantity:1
                  });
              }
+             saveToStorage();
+ }
+
+ export function removeFromCart(productId){
+    const newCart=[];
+    cart.forEach((cartItem)=>{
+        if(cartItem.productId !== productId ){
+            newCart.push(cartItem);
+        }
+    });
+
+    cart = newCart;
+
+    saveToStorage();
  }
