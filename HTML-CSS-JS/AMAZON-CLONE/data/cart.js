3e0c175da9bbecca0b14b576c579fb820bb908eb
Aman18-18 <amanssingh1827@gmail.com>
2025-07-02 16:52:31 +0530

    save data and generate html for delivery options

--- HTML-CSS-JS/AMAZON-CLONE/data/cart.js
+++ HTML-CSS-JS/AMAZON-CLONE/data/cart.js
@@ -1,3 +1,5 @@
+import { deliveryOptions, getDeliveryOption } from "./deliveryOptions";
+
  export let cart = JSON.parse(localStorage.getItem('cart'));
  
  
@@ -5,10 +7,12 @@
     cart =  [{
     productId : 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
     quantity : 2,
+    deliveryOptionId : '1'
  },
 {
     productId : '15b6fc6f-327a-4ec4-896f-486349e85a3d',
     quantity: 1,
+    deliveryOptionId : '2'
 }
 ];
   }
@@ -32,7 +36,8 @@ export function productQuantity(productId){
              }else{
                  cart.push({
                      productId :productId,
-                     quantity:1
+                     quantity:1,
+                     deliveryOptionId :'1'
                  });
              }
              saveToStorage();
@@ -47,6 +52,18 @@ export function productQuantity(productId){
     });
 
     cart = newCart;
+
+    saveToStorage();
+ }
+
+ export function updateDeliveryOption(productId,deliveryOptionId){
+    let matchingItem;
+    cart.forEach((cartItem)=>{
+        if(productId === cartItem.productId){
+            matchingItem = cartItem;
+        }
+    });
+    matchingItem.deliveryOptionId = getDeliveryOption;
 
     saveToStorage();
  }
